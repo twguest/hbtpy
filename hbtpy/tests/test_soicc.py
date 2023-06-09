464b69b99995609a9053f0f4cc65b75a740470e1
twguest <trey.guest@xfel.eu>
2023-06-09 18:18:18 +0200

    Formatting Updated

--- hbtpy/tests/test_soicc.py
+++ hbtpy/tests/test_soicc.py
@@ -3,10 +3,11 @@ import numpy as np
 
 from hbtpy.soicc import calculate_g2
 
+
 def test_g2():
     """
     test that the g2 calculator is functioning correctly
     """
-    i1 = np.ones([50,50])
+    i1 = np.ones([50, 50])
     i2 = i1
-    assert np.array_equal(calculate_g2(i1,i2), i1) is True, "Correlation Calculator Failed" 
+    assert np.array_equal(calculate_g2(i1, i2), i1) is True, "Correlation Calculator Failed"
